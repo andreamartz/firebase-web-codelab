db2a06581ccc983655643a8d8301fa0be3952939
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-19 13:37:44 -0600

    11 - Show notifications -  Add the FCM service worker

--- web-start/src/firebase-messaging-sw.js
+++ web-start/src/firebase-messaging-sw.js
@@ -1,1 +1,14 @@
+/** We'll now add support for browser notifications. The app will notify users when new messages are posted in the chat. Firebase Cloud Messaging (FCM) is a cross-platform messaging solution that lets you reliably deliver messages and notifications at no cost.
+ */
+
+// The service worker simply needs to load and initialize the Firebase Cloud Messaging SDK, which will take care of displaying notifications.
+
 // TODO: Set up Firebase Cloud Messaging service worker
+// Import and configure the Firebase SDK
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging/sw';
+import { getFirebaseConfig } from './firebase-config';
+
+const firebaseApp = initializeApp(getFirebaseConfig());
+getMessaging(firebaseApp);
+console.info('Firebase messaging service worker is set up');
